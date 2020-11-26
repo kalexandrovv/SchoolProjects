b673d1f4e5da5af241ae1e697dfdd2ef4f4080f4
kalexandrovv <72204673+kalexandrovv@users.noreply.github.com>
2020-11-26 14:31:07 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package paki4;
+
+public class Student {
+	public String name;
+	public double grade;
+	
+	public Student( ) {
+		
+	}
+	public Student (String name  , double grade) {
+		this.name = name;
+		this.grade = grade;
+	}
+	
+
+}
